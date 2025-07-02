1c670cd54dbf97bf0cae01c50e89918676f2e153
otavio <barbosaotavio293@gmail.com>
2025-07-02 02:24:20 -0300

    frist commit

---
+++ 3_OPERADORES/js/scripts.js
@@ -0,0 +1,105 @@
+//  Number
+// console.log(typeof 258)
+// console.log(typeof 25.6)
+// console.log(typeof 138)
+
+//  Operações aritiméticas
+
+// console.log(2+5) // adição
+// console.log(15-5) // subtração
+// console.log(15*3) // multiplicação
+// console.log(20/4) // divisão
+// console.log(5+(4*2)) //multiplicação com adição
+
+//  Especial numbers
+
+// console.log(typeof Infinity)
+// console.log(typeof -Infinity)
+// console.log(5/ "teste")
+// console.log(typeof NaN)
+
+
+// Strings
+
+// console.log('otavio')
+// console.log('idade')
+// console.log('29')
+
+// console.log(typeof 'otavio')
+// console.log(typeof 'idade')
+// console.log(typeof '29')
+
+// Concatenação
+
+// console.log('oi' + 'tudo' + 'bem?')
+// console.log(`testando` + `com` + `crase!`)
+
+// Interpolção(TemplateString)
+
+// console.log(`A soma de 2 + 2 é: ${2+2}`)
+
+// Boolean
+
+// console.log(true)
+// console.log(5 > 20)
+// console.log(20 > 5)
+
+//Comparação
+
+// console.log(15 == 5)
+// console.log(15 != 9)
+// console.log(15 != 15)
+
+// Comparação de Idêntico
+
+// console.log(9 == '9')
+// console.log(9 + '9')
+// console.log(9 === '9')
+
+// Operadores Lógicos
+
+/*
+
+|| OR - Para ser true, um lado com true ja basta.
+
+&& AND - true apenas se os dois lados forem verdadeiros.
+
+! Not - este operador inverte a comparação
+
+*/
+
+console.log(true && true)
+console.log(true && false)
+
+console.log(5 > 2 && 5 < 10)
+console.log(5 > 2 && 'Otavio' === 2)
+console.log(5 > 2  || 'Otavio' === 2)
+console.log(5 < 2 || 5 > 100)
+
+console.log(!true)
+console.log(5 > 2)
+console.log(!5 > 2)
+
+// Empty values 
+
+// null - Ainda não tem um valor, podendo ser modificado.
+
+console.log(typeof null, typeof undefined)
+
+console.log(null === undefined)
+console.log(null == undefined)
+
+console.log(null == false)
+console.log(undefined == false)
+
+// Mudança de tipos 
+
+console.log(5 * null)
+console.log('otavio' * 'barbosa')
+console.log('10' + 1)
+console.log('10' - 9)
+
+
+
+
+
---
+++ ESTRUTURAS/js/scripts.js
@@ -0,0 +1,146 @@
+// Variáveis 
+let nome ='Otavio'
+console.log(nome)
+
+// Com a palavra 'let' podemos alterar o valor da variavel.
+
+nome = 'Otavio Barbosa'
+console.log(nome)
+
+const idade = 29
+console.log(idade)
+
+// Com a plavra 'const' o valor da variavel não pode ser alterada.
+
+// idade = 35
+// console.log(idade)
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+// Mais sobre variáveis 
+
+// let 2teste = 'inválido'
+// let @teste = 'inválido'
+
+// let a = 10, b = 20, c =30
+// console.log(a, b, c) 
+
+// const nomeCompleto = 'Otavio Barbosa'
+// const nomecompleto = 'Alice Barbosa'
+// console.log(nomeCompleto)
+// console.log(nomecompleto)
+
+// const _teste ='ok'
+// const $teste ='ok'
+// console.log(_teste, $teste)
+
+// const age = prompt ('digite sua idade')
+// console.log(`você tem ${age} anos de idade.`)
+
+// alert('testando')
+
+// const z = 10 
+
+// alert(`o número é ${z}`)
+
+
+// math
+
+// console.log(Math.max(5, 2, 1, 10, 15))
+
+// console.log(Math.floor(5.14))
+
+// console.log(Math.ceil(5.14))
+
+
+// console.log('teste!')
+// console.error('erro!')
+// console.warn('aviso!')
+
+// if 
+
+const m = 10
+
+if(m > 5){
+    console.log('m é maior que 5!')
+}
+
+const user = 'João'
+
+if(user === 'João') {
+    console.log(`Olá ${user}`)
+}
+
+if( user === 'Maria'){
+    console.log(`Olá ${user}`) 
+}
+
+console.log(user === 'João', user === 'Maria')
+
+
+// else
+
+const loggedIn = false
+
+if(loggedIn){
+    console.log('Está autenticado!')
+} else{
+    console.log('Não está autenticado!')
+}
+
+const q = 10
+const w = 25
+
+if(q > 5 && w > 20){
+    console.log('Números são mais altos!')
+} else{
+    console.log('Os números não são mais altos!')
+}
+
+const cabelo = 'preto'
+
+
+if(cabelo === 'preto'){
+    console.log('Seu cabelo é preto!')
+} else {
+    console.log('Seu cabelo não é preto !')
+}
+
+const pais = 'Brasil'
+
+if(pais === 'Argentina'){
+    console.log('Você é Brasileiro!')
+} else{
+    console.log('Você é estrangeiro!')
+}
+
+const camisa = 'M'
+
+if(camisa == 'M'){
+    console.log('Sua camisa é M')
+} else{
+    console.log('Sua camisa não é M')   
+}
+
+// else if 
+
+if(1 > 2){
+    console.log('Teste')
+} else if(2 > 3){
+    console.log('Teste 2')
+} else if(5 > 2){
+    console.log('Agora sim')
+}
+
+const userName = 'Otavio'
+const userAge = 29
+
+if(userName === 'Carlos'){
+    console.log(`Bem vindo ${userName}`)
+}
+else if(userName === 'Otavio' && userAge === 29){
+    console.log(`Olá ${userName} sua idade é ${userAge}`)
+} else{
+    console.log('Nenhuma condição aceita!')
+}
