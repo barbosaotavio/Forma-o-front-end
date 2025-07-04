4e74c7b76ba734f49ab53c49038ccee06592328f
otavio <barbosaotavio293@gmail.com>
2025-07-04 01:43:44 -0300

    Função

---
+++ FUNCOES/js/scripts.js
@@ -0,0 +1,21 @@
+// Criando uma função
+
+function minhaFuncao(){
+    console.log('testando')
+}
+
+minhaFuncao()
+minhaFuncao()
+
+const minhaFuncaoEmVariavel = function(){
+    console.log('Função em variável')
+}
+
+minhaFuncaoEmVariavel()
+
+function funcaoComParametro(txt){
+    console.log(`Imprimindo: ${txt}`)
+}
+
+funcaoComParametro('Imprimindo alguma coisa.')
+funcaoComParametro('Outra função')
