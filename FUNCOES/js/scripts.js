3ec8ac137cb665748a52cddb161f3cefee0e40cc
otavio <barbosaotavio293@gmail.com>
2025-07-05 00:36:56 -0300

    arrow function

--- FUNCOES/js/scripts.js
+++ FUNCOES/js/scripts.js
@@ -36,6 +36,7 @@ console.log(resultado)
 
 console.log(soma(c, d))
 
+// 
 
 const f = 42
 const g = 22
@@ -47,3 +48,87 @@ function somar(n1, n2){
 console.log(somar(f, g))
 
 
+// escopo da função
+
+let y = 10
+function testandoEscopo(){
+    let y = 20
+    console.log(`Y dentro da função é ${y}`)
+}
+y = 15
+
+console.log(`Y fora da função é ${y}`)
+
+testandoEscopo()
+
+
+let h = 25
+
+function escopo(){
+    let h = 45
+    console.log(`O valor de h é ${h}`)
+}
+console.log(`H fora da função vale: ${h}`)
+escopo()
+
+// escopo aninhado
+
+let m = 10
+
+function escopoAninhado(){
+    let m = 20
+    if(true){
+
+        let m = 30
+        if(true){
+
+            let m = 40
+            console.log(m)
+        }
+        console.log(m)
+    }
+    console.log(m)
+}
+escopoAninhado()
+console.log(m)
+
+
+// arrow function
+
+const testeArrow = () => {
+    console.log('Esta é uma arrow function')
+}
+
+testeArrow()
+
+const parOuImpar = (n) => {
+    if(n % 2 === 0){
+        console.log('Par')
+        return
+    }
+        console.log('Impar')
+}
+
+parOuImpar(5)
+parOuImpar(10)
+parOuImpar(15)
+parOuImpar(22)
+
+// mais sobre arrow function
+
+const raizQuadrada = (x) => {
+   return x * x 
+}
+
+console.log(raizQuadrada(4))
+
+const raizQuadrada2 = (x) => x * x 
+
+console.log(raizQuadrada2(5))
+console.log(raizQuadrada2(12))
+console.log(raizQuadrada2(6))
+
+
+const helloWorld = () => console.log('Hello World')
+
+helloWorld()
